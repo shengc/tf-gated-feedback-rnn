10dda4b0ab92e3ee9578637a9108af341221b1ee
leakyabstraction <abstractionleakage@gmail.com>
2017-10-31 14:56:00 -0400

    added gru cell for gated feedback rnn and updated the notebook accordingly

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # gated-feedback-rnn
-a practical implementation of Gated Feedback RNN: https://arxiv.org/abs/1502.02367
+a practical implementation of Gated Feedback RNN for both LSTM and GRU: https://arxiv.org/abs/1502.02367
 
 While serving the purpose of exploring the idea from the paper, the major goal of this project is to implement the gated feedback rnn as close to other RNN implemention in Tensorflow's source code as possible. It should make the best use of what Tensor has to offer, and can be integrated into normal procedure of running an RNN in Tensorflow seamlessly, e.g., running with `tf.nn.dynamic_run`. 
--- gfr.py
+++ gfr.py
@@ -1,9 +1,10 @@
-from tensorflow.python.ops.rnn_cell import BasicLSTMCell, LSTMStateTuple, RNNCell
+from tensorflow.python.ops.rnn_cell import BasicLSTMCell, LSTMStateTuple, RNNCell, GRUCell
 from tensorflow.python.ops.rnn_cell_impl import _linear
 from tensorflow.python.ops import array_ops, math_ops
 from tensorflow.python.ops.variable_scope import variable_scope
 from tensorflow.python.framework.ops import name_scope
 from tensorflow.contrib.framework import nest
+from tensorflow import ones_initializer
 
 class GatedFeedbackLSTMCell(BasicLSTMCell):
     def __init__(self, num_units, layer_pos, forget_bias=1.0, activation=None, reuse=None, kernel_initializer=None, bias_initializer=None):
@@ -18,7 +19,7 @@ class GatedFeedbackLSTMCell(BasicLSTMCell):
         c, h = state[self._layer_pos]
         concat_h = array_ops.concat([s[-1] for s in state], axis=1)
 
-        with variable_scope('input-forget-output-gate'):
+        with variable_scope('input-forget-output-gates'):
             conc = _linear([inputs, h], 3 * self._num_units, True, bias_initializer=self._bias_initializer, kernel_initializer=self._kernel_initializer)
             i, f, o = array_ops.split(conc, 3, axis=1)
         with variable_scope('scalar-gates'):
@@ -28,11 +29,38 @@ class GatedFeedbackLSTMCell(BasicLSTMCell):
                 _linear(array_ops.reshape(array_ops.expand_dims(gates, axis=2) * array_ops.expand_dims(h, axis=1), (-1, n_layer * self._num_units)),
                         self._num_units, True, bias_initializer=self._bias_initializer, kernel_initializer=self._kernel_initializer)
         with variable_scope('new-inputs'):
-            new_inputs = _linear(inputs, self._num_units, True, bias_initializer=self._bias_initializer, kernel_initializer=self._kernel_initializer) + gated_h
-        new_c = c * math_ops.sigmoid(f + self._forget_bias) + math_ops.sigmoid(i) * self._activation(new_inputs)
-        new_h = self._activation(new_c) * math_ops.sigmoid(o)
+            new_inputs = \
+                self._activation(_linear(inputs, self._num_units, True, bias_initializer=self._bias_initializer, kernel_initializer=self._kernel_initializer) + gated_h)
+        new_c = self._activation(c * math_ops.sigmoid(f + self._forget_bias) + math_ops.sigmoid(i) * new_inputs)
+        new_h = new_c * math_ops.sigmoid(o)
         new_state = LSTMStateTuple(new_c, new_h)
         return (new_h, new_state)
+
+class GatedFeedbackGRUCell(GRUCell):
+    def __init__(self, num_units, layer_pos, activation=None, reuse=None, kernel_initializer=None, bias_initializer=None):
+        super(GatedFeedbackGRUCell, self).__init__(num_units=num_units, activation=activation, reuse=reuse, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer)
+        self._layer_pos = layer_pos
+    def call(self, inputs, state):
+        n_layer = len(state)
+        h = state[self._layer_pos]
+        concat_h = array_ops.concat(state, axis=1)
+
+        with variable_scope('reset-output-gates'):
+            dtype = next(a.dtype for a in [inputs, h])
+            bias_ones = ones_initializer(dtype=dtype) if self._bias_initializer is None else self._bias_initializer
+            value = math_ops.sigmoid(_linear([inputs, h], 2 * self._num_units, True, bias_ones, self._kernel_initializer))
+            r, u = array_ops.split(value=value, num_or_size_splits=2, axis=1)
+        with variable_scope('scalar-gates'):
+            gates = _linear([inputs, concat_h], n_layer, True, bias_initializer=self._bias_initializer, kernel_initializer=self._kernel_initializer)
+        with variable_scope('gated-inputs'):
+            gated_h = \
+                _linear(array_ops.reshape(array_ops.expand_dims(gates, axis=2) * array_ops.expand_dims(h, axis=1), (-1, n_layer * self._num_units)),
+                        self._num_units, True, bias_initializer=self._bias_initializer, kernel_initializer=self._kernel_initializer) * r
+        with variable_scope('new-inputs'):
+            new_inputs = \
+                self._activation(_linear(inputs, self._num_units, True, bias_initializer=self._bias_initializer, kernel_initializer=self._kernel_initializer) + gated_h)
+        new_h = u * h + (1 - u) * new_inputs
+        return new_h, new_h
 
 class MultiGatedFeedbackRNNCell(RNNCell):
     def __init__(self, cells):
